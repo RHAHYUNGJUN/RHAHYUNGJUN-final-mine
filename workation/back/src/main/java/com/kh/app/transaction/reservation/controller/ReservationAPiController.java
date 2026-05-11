17511a24f082b46d30cbc9442f60fa5bbc259d30
DESKTOP-3CV852R\user1 <bo>
2026-05-11 16:31:51 +0900

    feat : reserve CRUD/김민성

--- workation/back/src/main/java/com/kh/app/transaction/reservation/controller/ReservationAPiController.java
+++ workation/back/src/main/java/com/kh/app/transaction/reservation/controller/ReservationAPiController.java
@@ -1,4 +1,41 @@
 package com.kh.app.transaction.reservation.controller;
 
-public class ReservationAPiController {
+import com.kh.app.transaction.reservation.dto.request.ReservationCreateReqDto;
+import com.kh.app.transaction.reservation.dto.response.ReservationResDto;
+import com.kh.app.transaction.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservation")
+@RequiredArgsConstructor
+public class ReservationApiController {
+
+    private final ReservationService reservationService;
+
+    @PostMapping
+    public Long create(
+            @RequestPart("dto") ReservationCreateReqDto dto,
+            @RequestPart(value = "files", required = false) List<MultipartFile> files
+    ) {
+        return reservationService.create(dto, files);
+    }
+
+    @GetMapping("/{id}")
+    public ReservationResDto getOne(@PathVariable Long id) {
+        return reservationService.getOne(id);
+    }
+
+    @PostMapping("/{id}/cancel")
+    public void cancel(@PathVariable Long id) {
+        reservationService.cancel(id);
+    }
+
+    @PostMapping("/{id}/complete")
+    public void complete(@PathVariable Long id) {
+        reservationService.complete(id);
+    }
 }
