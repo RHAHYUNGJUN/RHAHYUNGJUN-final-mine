425c8272bbc59496c84d48a362e386392a4da8ec
DESKTOP-DB3FB27\user1 <wook's>
2026-05-12 17:50:12 +0900

    feat : space controller  / service insert init / 영욱

--- workation/back/src/main/java/com/kh/app/product/space/controller/SpaceApiController.java
+++ workation/back/src/main/java/com/kh/app/product/space/controller/SpaceApiController.java
@@ -1,8 +1,12 @@
 package com.kh.app.product.space.controller;
 
+import com.kh.app.product.space.dto.request.SpaceInsertReqDto;
+import com.kh.app.product.space.service.SpaceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SpaceApiController {
 
-    @PostMapping
-    public void m01(){
+    private final SpaceService spaceService;
 
+    @PostMapping
+    public ResponseEntity<Long> insert(
+            @RequestBody SpaceInsertReqDto reqDto
+    ) {
+
+        Long id = spaceService.insert(reqDto);
+
+        return ResponseEntity.ok(id);
     }
 }
