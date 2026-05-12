bb605d2526fa7804ffd858413c6b550739271812
DESKTOP-3CV852R\user1 <bo>
2026-05-12 17:27:37 +0900

    fix : 예약하기 쿠폰 스테이 오피스 받아오기 수정요함

--- workation/back/src/main/java/com/kh/app/transaction/reservation/controller/ReservationApiController.java
+++ workation/back/src/main/java/com/kh/app/transaction/reservation/controller/ReservationApiController.java
@@ -1,18 +1,20 @@
 package com.kh.app.transaction.reservation.controller;
 
+import com.kh.app.security.user.CustomUserDetails;
 import com.kh.app.transaction.reservation.dto.request.ReservationCreateReqDto;
 import com.kh.app.transaction.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/reservation")
+@RequestMapping("/api/user/reservation")
 @RequiredArgsConstructor
 @Slf4j
 public class ReservationApiController {
@@ -21,10 +23,11 @@ public class ReservationApiController {
 
     @PostMapping
     public ResponseEntity<Long> create(
-            @RequestPart("dto") ReservationCreateReqDto dto,
-            @RequestPart(value = "files", required = false) List<MultipartFile> files
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @ModelAttribute("dto") ReservationCreateReqDto dto,
+            @RequestParam(value = "files", required = false) List<MultipartFile> files
     ) {
-        Long reservationId = reservationService.create(dto, files);
+        Long reservationId = reservationService.create(userDetails,dto, files);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
